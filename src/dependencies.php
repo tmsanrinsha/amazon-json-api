955552a4523acaadbf4e314ef569ff4ed9b5827a
tmsanrinsha <sanrinsha.tm@gmail.com>
2017-05-28 15:32:54 +0900

    リトライ処理の追加

--- src/dependencies.php
+++ src/dependencies.php
@@ -5,6 +5,15 @@ use ApaiIO\Configuration\GenericConfiguration;
 use ApaiIO\Operations\Lookup;
 use ApaiIO\ApaiIO;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
 $container = $app->getContainer();
 
 $container['lookup'] = function ($c) {
@@ -12,7 +21,54 @@ $container['lookup'] = function ($c) {
 };
 
 $container['apaiio'] = function ($c) {
-    $client = new \GuzzleHttp\Client();
+    //リトライ判断
+    $decider = function (
+        $retries,
+        Request $request,
+        Response $response = null,
+        RequestException $e = null
+    ) use ($c) {
+        if ($retries >= 5) {
+            return false;
+        }
+
+        $shouldRetry = false;
+
+        if ($e instanceof ConnectException) {
+            $shouldRetry = true;
+        }
+
+        if ($response) {
+           if ($response->getStatusCode() >= 500) {
+               $shouldRetry = true;
+           }
+        }
+
+        if ($shouldRetry) {
+            $c->logger->warning(
+                sprintf(
+                    'Retrying %s %s %s/5, %s',
+                    $request->getMethod(),
+                    $request->getUri(),
+                    $retries + 1,
+                    $response ? 'status code: ' . $response->getStatusCode() :
+                    $e->getMessage()
+                )
+            );
+        }
+
+        return $shouldRetry;
+    };
+
+    //遅延時間を返す
+    $delay = function ($retries) {
+        return 1000; // 1000ミリ秒待つ
+    };
+
+    $handlerStack = HandlerStack::create(new CurlHandler());
+    $handlerStack->push(Middleware::retry($decider, $delay));
+    $client = new Client(['handler' => $handlerStack, 'timeout' => 5]);
+
     $request = new \ApaiIO\Request\GuzzleRequest($client);
 
     $conf = new GenericConfiguration();
